f3509b1f1e89139dbcc1deb4b507de3f3cfb4cff
nielmerzaaa <reinieljohnmerza@gmail.com>
2026-02-20 05:21:22 +0800

    initial commit for gitbashtest file

---
+++ gitbashtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "What is your Name? "
+read -r first_Name
+echo "How old are you? "
+read -r age
+
+echo "Nice to meet you $first_Name. You're already $age years old thats nice"
+
